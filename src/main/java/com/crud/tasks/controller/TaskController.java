78fee8624ef96f13a53c6ddb9057b0336f87dcb7
Kamil <kamil.krol@gmail.com>
2021-08-16 23:56:31 +0200

    Modul 19.2 niedokończony

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,12 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.taks.service.DbService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,10 +14,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+
+
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public  List<TaskDto> getTasks() {
-        return new ArrayList<>();
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
@@ -29,6 +44,9 @@ public class TaskController {
     }
     @PostMapping(value = "createTask")
     public void createTask(TaskDto taskDto) {
+
+
+
 
     }
 }
