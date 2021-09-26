61f524db252890a605544a669f8a27f76a531e6d
Kamil <kamil.krol@gmail.com>
2021-09-27 00:35:02 +0200

    Modul22.4

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -2,18 +2,20 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
 
     @JsonProperty("id")
-    private String id;
+    private  String id;
 
     @JsonProperty("name")
-    private String name;
+    private  String name;
 
     @JsonProperty("shortUrl")
-    private String shortUrl;
+    private  String shortUrl;
 }
