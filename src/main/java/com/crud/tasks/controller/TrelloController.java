e22142e8894e8112de5be96e56bed1f618b950d5
Kamil <kamil.krol@gmail.com>
2021-09-26 19:00:13 +0200

    Zadanie 22.3 v1

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.AttachmentsByType;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
