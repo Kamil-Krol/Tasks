cc0dce2c9236b85ff83b2c133ae10dee508a237b
Kamil <kamil.krol@gmail.com>
2021-09-27 23:57:40 +0200

    Zadanie 23.3 v1

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,12 +1,22 @@
 package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Optional;
+
+@Builder
 @Getter
 @AllArgsConstructor
 public class Mail {
     private final String mailTo;
     private final String subject;
     private final String message;
+    private final Optional<String> toCc;
+
+    public static MailBuilder builder()
+    {
+        return new MailBuilder();
+    }
 }
